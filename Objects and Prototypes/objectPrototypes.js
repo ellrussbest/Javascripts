005ff8a50a068f4ea6c1de0055ead2b533ca8a09
ellrussbest <72700083+ellrussbest@users.noreply.github.com>
2023-05-27 01:54:20 +0200

    Create objectPrototypes.js

---
+++ Objects and Prototypes/objectPrototypes.js
@@ -0,0 +1,32 @@
+// Prototypes
+// Whenever you create a function it will automatically create a prototype
+// object within the function e.g.
+function foo() {}
+foo.prototype
+
+// Whenever you create an object using the new keyword and a function
+// the object will automatically create __proto__ which will point to the
+// function's prototype e.g.
+const bar = new foo()
+bar.__prototype__
+
+// these two things point to the same object i.e
+foo.prototype === bar.__prototype__
+
+console.log(foo.prototype === bar.__proto__)
+
+
+// This is what happens under the hood
+/** _________________________   **/
+
+function brr() {
+    const pr = {}; // this represents the prototype
+    this.__pr__ = this.abstract; // this represents the __protoype__ 
+}
+
+
+const arr = new brr() // this will create __protype__ for the new object
+
+console.log(arr.__pr__ === brr.pr) // evaluates to true
+
+console.log(arr.abstract) // evaluates to undefined
