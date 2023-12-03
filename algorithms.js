9b63a52831c0d1e9cdbb14623b8ec6b07159db30
ellrussbest <ellrussbest@gmail.com>
2023-12-03 22:17:28 +0100

    cycle

--- algorithms.js
+++ algorithms.js
@@ -67,4 +67,40 @@ bubbleSort(arr3)
 console.log(arr3)
 
 // Floyd's Tortoise and Hare Algorithm
+arr = [1, 1, 2, 2, 3, 4]
 
+function hasCycle(arr) {
+    slow = 0
+    fast = 0
+
+    while((fast < arr.length) && (fast + 1 < arr.length)) {
+        slow += 1
+        fast += 2
+
+        if (slow === fast) return true
+    }
+
+    return false
+}
+
+console.log(hasCycle(arr))
+
+// boolean hasCycle(ListNode head) {
+//     ListNode slow = head, fast = head;
+
+//     while(fast != null && fast.next != null) {
+//         slow = slow.next
+//         fast = fast.next.next
+
+//         if(slow == fast) return true
+//     }
+//     return false
+// }
+
+// 1. Initially, both the slow and fast pointers are at the head of the linked list.
+// 2. In each iteration of the while loop, the slow pointer moves one step forward (slow = slow.next), and the fast pointer moves two steps forward (fast = fast.next.next).
+// 3. If there is no cycle in the linked list, the fast pointer will reach the end of the list (fast == null) before catching up to the slow pointer.
+// 4. However, if there is a cycle, the fast pointer will eventually enter the cycle and start "lapping" the slow pointer.
+// 5. As the fast pointer moves two steps at a time, and the slow pointer moves one step at a time, they will eventually meet at the same node within the cycle (slow == fast).
+// 6. If the slow and fast pointers meet, it means there is a cycle in the linked list, and we return true.
+// 7. If the fast pointer reaches the end of the list (fast == null) without meeting the slow pointer, it means there is no cycle, and we return false.
